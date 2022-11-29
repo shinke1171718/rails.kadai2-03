d42ec7d94dfc403e42d0483fba9a366bb824fbae
Apple <apple@ShinkenoMacBook.local>
2022-11-29 20:06:38 +0900

    roomのnewアクション記述＋view画面作成

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get rooms_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get rooms_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get rooms_create_url
+    assert_response :success
+  end
 end
